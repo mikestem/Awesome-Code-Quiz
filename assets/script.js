02f26f8b9b8afc0151e980d375435c6b7c346361
mikestem <michael.a.stem@gmail.com>
2022-12-14 23:18:24 -0500

    added questions

--- assets/script.js
+++ assets/script.js
@@ -1,2 +1,28 @@
-var timer = document.querySelector("#timer");
-var questions = document.querySelector("#questionsList");
+var timer = document.querySelector("#timeRemaining");
+var questionsList = document.querySelector("#questionsList");
+var start = document.querySelector("#start")
+
+
+var questions = [
+    {
+        title: "What element is used to store multiple values in a single variable?"
+        choices: ["functions", "strings", "arrays", "variables"]
+        answer: "arrays"
+    },
+    {
+        title: "What is a block of code called that is used to perform a specific task?"
+        choices: ["string", "function", "boolean", "null"]
+        answer: "function"
+    },
+    {
+        title: "One loop inside the body of another loop is called"
+        choices: ["double loops", "confusing", "nested", "no word for it"]
+        answer: "nested"
+    },
+    {
+        title: "Which of the following in NOT a Primitive data type"
+        choices: ["string", "Nnumber", "boolean", "object"]
+        answer: "object" 
+    },
+
+];
